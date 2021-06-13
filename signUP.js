3904f7f78e57012f95540e9359526f4e9a1c8f37
yomnaalwattar <66415204+yomnaalwattar@users.noreply.github.com>
2021-06-13 19:45:26 +0200

    Add files via upload

---
+++ signUP.js
@@ -0,0 +1,41 @@
+function checkPassword(form) {
+    if (form.pw.value.length != 8) {
+        alert("Error: password must contain 8 characters! Check the password title, please.");
+        return false;
+    }
+
+    if (form.pw.value.charAt(0) != form.pw.value.charAt(0).toUpperCase()
+        || Number(form.pw.value.charAt(0)) || form.pw.value.charAt(0) == "-"
+        || form.pw.value.charAt(0) == "_" || form.pw.value.charAt(0) == "*" ||
+        form.pw.value.charAt(0) == "#" || form.pw.value.charAt(0) == "&" ||
+        form.pw.value.charAt(0) == " ") {
+        alert("Error: password must start with uppercase letter (A-Z)! Check the password title, please.");
+        return false;
+
+    }
+    var i = "1";
+    while (!Number(form.pw.value.charAt(i))) {
+        i++;
+        if (i == 8) {
+            alert("Error: password must include at least one digit (0-9)! Check the password title, please.");
+            return false;
+            break;
+        }
+        else continue;
+    }
+    var i = "1";
+    while (form.pw.value.charAt(i) != "-" && form.pw.value.charAt(i) != "_" &&
+        form.pw.value.charAt(i) != "#" && form.pw.value.charAt(i) != "&" &&
+        form.pw.value.charAt(i) != "*" || form.pw.value.charAt(i) == " ") {
+        i++;
+        if (i == 8) {
+            alert("Error: password must include a special character and no spaces! Check the password title, please.");
+            return false;
+            break;
+        }
+        else continue;
+    }
+    alert("You entered a valid password: " + form.pw.value);
+    alert("Thank you  for your submition " + form.name1.value + " ,we'll send you notifications to this new added email.")
+    return true;
+}
